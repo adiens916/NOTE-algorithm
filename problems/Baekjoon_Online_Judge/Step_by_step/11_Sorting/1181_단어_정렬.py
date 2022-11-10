37777febf1b6674b0e3cfbd0e635e82aff45cead
Inho <adiens916@naver.com>
2022-11-11 07:10:18 +0900

    feat: Change 1181 단어 정렬

--- problems/Baekjoon_Online_Judge/Step_by_step/11_Sorting/1181_단어_정렬.py
+++ problems/Baekjoon_Online_Judge/Step_by_step/11_Sorting/1181_단어_정렬.py
@@ -1,6 +1,9 @@
 """
 우선 문자 길이에 맞는 배열을 찾은 후,
 그 다음 문자를 사전순으로 비교하며 삽입 정렬함.
+
+=> 시간 초과남. 삽입 정렬의 시간 복잡도로는 역시 부족한 듯.
+계산했을 때도 O(logN)은 되어야 했으니까...
 """
 
 from pathlib import Path
@@ -14,54 +17,16 @@ sys.stdin = open(f"{parent}/{filename}_input.txt")
 input = sys.stdin.readline
 
 
-MAX_LEN = 50
-
-
 def main():
     N = int(input())
     words = [input().rstrip() for _ in range(N)]
-    sorted_words = sort_words_by_length(words, N)
+    words = list(set(words))
 
-    for word_list in sorted_words[1:]:
-        for word in word_list:
-            if word:
-                print(word)
-            else:
-                break
+    words_with_len = [(len(word), word) for word in words]
+    words_with_len.sort()
 
-
-def sort_words_by_length(words: list[str], N) -> list[str]:
-    words_by_len = [[""] * N for _ in range(MAX_LEN + 1)]
-
-    for word in words:
-        length = len(word)
-        for i in range(N):
-            comp_word = words_by_len[length][i]
-            # 배열의 빈 곳에는 무조건 넣음
-            # 예시: 맨 처음, 맨 마지막인 경우 등
-            if comp_word == "":
-                words_by_len[length][i] = word
-                break
-            # 중복이면 넘어감
-            elif word == comp_word:
-                break
-            # 비교 중인 단어보다 사전순으로 먼저 오는 경우
-            elif word < comp_word:
-                # 삽입 정렬 이용
-                inserting = word
-                for j in range(i, N):
-                    # 기존 단어 보존
-                    preserving = words_by_len[length][j]
-                    if inserting:
-                        # 기존 위치에 새 단어 넣음
-                        words_by_len[length][j] = inserting
-                        # 새로 넣을 단어를 아까 보존했던 단어로 바꿈
-                        inserting = preserving
-                    else:
-                        break
-                break
-
-    return words_by_len
+    for length, word in words_with_len:
+        print(word)
 
 
 main()
