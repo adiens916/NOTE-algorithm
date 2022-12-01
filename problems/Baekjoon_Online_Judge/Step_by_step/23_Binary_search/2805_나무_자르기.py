0d163f0a4998f53d922c1f126b278767516b3fc1
Inho <adiens916@naver.com>
2022-12-02 08:23:26 +0900

    [feat] Improve 2805. 나무 자르기

--- problems/Baekjoon_Online_Judge/Step_by_step/23_Binary_search/2805_나무_자르기.py
+++ problems/Baekjoon_Online_Judge/Step_by_step/23_Binary_search/2805_나무_자르기.py
@@ -9,8 +9,28 @@ input = sys.stdin.readline
 def main():
     N, M = map(int, input().split())
     trees = list(map(int, input().split()))
-    cut_height = binary_search_for_min_sum_of_cut(trees, M)
+    cut_height = simple_binary_search(trees, M)
     print(cut_height)
+
+
+def simple_binary_search(trees: list[int], M: int) -> int:
+    # 참고: https://www.acmicpc.net/source/50932012
+
+    min_range = 0
+    max_range = max(trees)
+
+    while min_range <= max_range:
+        mid = (min_range + max_range) // 2
+        # 튜플로 생성할 경우, 리스트에 비해 400ms 단축 (1000ms -> 600ms)
+        sum_of_cut = sum((tree - mid for tree in trees if tree >= mid))
+
+        if sum_of_cut < M:
+            max_range = mid - 1
+        else:
+            min_range = mid + 1
+
+    # 최대 범위만 반환하면 된다???
+    return max_range
 
 
 def binary_search_for_min_sum_of_cut(trees: list[int], M: int) -> int:
