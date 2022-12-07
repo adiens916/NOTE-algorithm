cf6d6e0127ccf5e07234ca941e1fc836f4eae267
Inho <adiens916@naver.com>
2022-12-07 16:00:03 +0900

    [feat] Complete 18111. 마인크래프트

--- problems/Baekjoon_Online_Judge/18111_마인크래프트.py
+++ problems/Baekjoon_Online_Judge/18111_마인크래프트.py
@@ -6,9 +6,6 @@ sys.stdin = open(input_txt)
 input = sys.stdin.readline
 
 
-MAX_WORK_TIME = 256 * 25000 * 2
-
-
 def main():
     N, M, B = map(int, input().split())
     lands = [list(map(int, input().split())) for _ in range(N)]
@@ -33,31 +30,9 @@ def brute_force_for_min_work_time(lands, B):
     return min_work_time, heights[-1]
 
 
-# def binary_search_by_min_work_time(lands, B) -> tuple[int, int]:
-#     """
-#     이진 탐색은 불가: 작업 시간과 높이에는 상관 관계가 없었음?...
-#     """
-#     min_range = min((min(line) for line in lands))
-#     max_range = max((max(line) for line in lands))
-#     best_work_time = MAX_WORK_TIME
-#     best_height = 0
-
-#     while min_range <= max_range:
-#         mid = (min_range + max_range) // 2
-#         work_time = dig_then_put_for_height_with_blocks(lands, mid, B)
-
-#         # 작업 시간이 같으면 최대 높이로
-#         if work_time <= best_work_time:
-#             min_range = mid + 1
-#             best_work_time = work_time
-#             best_height = mid
-#         else:
-#             max_range = mid - 1
-
-#     return best_work_time, best_height
-
-
 def dig_then_put_for_height_with_blocks(lands, flat, B) -> int:
+    # NOTE: MAX 값을 잘못 설정하니까 틀렸었음.
+    MAX_WORK_TIME = 500 * 500 * 256 * 2
     work_time = 0
 
     for line in lands:
