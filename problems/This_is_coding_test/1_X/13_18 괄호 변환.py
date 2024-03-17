be4e1ac0a5699cef46bc3d7c1bbb820695106f24
Inho <adiens916@naver.com>
2024-03-18 06:29:10 +0900

    13_17 경쟁적 전염, 13_18 괄호 변환

---
+++ problems/This_is_coding_test/1_X/13_18 괄호 변환.py
@@ -0,0 +1,70 @@
+# XXX: left, right 안 쓰고 stack만 가지고도 올바름 판별 가능
+# left면 stack ++, right면 stack --
+# stack이 음수가 된 적이 한 번이라도 있으면 매칭 X
+# stack이 0이 되면 좌우 개수 같음
+# 참고: https://school.programmers.co.kr/learn/courses/30/lessons/60058/solution_groups?language=python3
+
+
+def solution(p: str) -> str:
+    answer = fix(p)
+    return answer
+
+
+def fix(s: str) -> str:
+    # XXX: 빈 문자열이면 빈 문자열 반환
+    if s == "":
+        return ""
+
+    u, v, is_u_right = partition(s)
+    if is_u_right:
+        return u + fix(v)
+    else:
+        return "(" + fix(v) + ")" + reverse_p(u)[1:-2]
+
+
+def partition(s: str) -> tuple:
+    stack = 0
+    left = 0
+    right = 0
+
+    for i in range(len(s)):
+        if s[i] == "(":
+            left += 1
+            stack += 1
+        else:
+            right += 1
+            # XXX: 스택이 (가 채워져 있는 경우에만
+            if stack > 0:
+                # 쌍 이뤄서 제거하고 스택 감소
+                stack -= 1
+
+        if left == right:
+            u = s[: i + 1]
+            v = s[i + 1 :]
+            # 스택이 빈 경우면 올바른 문자열임
+            is_right = stack == 0
+            return u, v, is_right
+
+
+def reverse_p(s: str) -> str:
+    # XXX: 문자열을 뒤집는 게 아니라, 괄호를 뒤집어야 함
+    # 예) ))(()( 주어진 경우,
+    # 문자열을 뒤집으면 ()(())
+    # 괄호를 뒤집으면 (())()
+    r = []
+    for c in s:
+        if c == "(":
+            r.append(")")
+        else:
+            r.append("(")
+    return "".join(r)
+
+
+"""
+()))((()
+# ()(())()
+"""
+"""
+(()())()
+# (()())()
+"""
