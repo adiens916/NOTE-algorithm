476cadfaa3a4de94e3bd5c9c87c8bb74bcaf4a72
Inho <adiens916@naver.com>
2024-05-21 06:37:31 +0900

    18_42 탑승구

--- problems/This_is_coding_test/1_X/18_42 탑승구.py
+++ problems/This_is_coding_test/1_X/18_42 탑승구.py
@@ -8,22 +8,43 @@ G = int(input())
 P = int(input())
 flights = [int(input()) for _ in range(P)]
 
+# 탑승구 정보를 루트 노드로 가짐
+parents = [i for i in range(G + 1)]
 
 count = 0
-parents = [i for i in range(G + 1)]
 for flight in flights:
-    available = find_parent(flight, parents)
-    if available == 0:
-        print(count)
+    # 해당 탑승구 정보 가져옴
+    parent = find_parent(flight, parents)
+    # 만약 탑승구가 0을 가리키면 (즉, 남는 탑승구가 없으면) 종료
+    if parent == 0:
         break
     else:
-        # 루트까지 가는 곳 전부 갱신하기
-        # 2에서 1 줄이고
-        # 3의 루트를 1로 함
-        # (2의 루트도 1로 함)
+        # 가능한 탑승구 숫자를 줄여야 함
+        # 일단 루트 노드의 숫자도 한 개 줄이고
+        parents[parent] = parent - 1
+        # 루트 노드가 옮겨 갔으니, 현재 탑승구의 루트 노드도 갱신
+        parents[flight] = parent - 1
         count += 1
 
+print(count)
 
+"""
+4
+3
+4
+1
+1
+"""  # 2
+"""
+4
+6
+2
+2
+3
+3
+4
+4
+"""  # 3
 """
 3
 3
