69e43c5d80c82fcc509dc6ed705c7b490663e310
Inho <adiens916@naver.com>
2024-05-22 07:36:25 +0900

    18_44 행성 터널

--- problems/This_is_coding_test/1_X/18_44 행성 터널.py
+++ problems/This_is_coding_test/1_X/18_44 행성 터널.py
@@ -17,17 +17,22 @@ def union(a: int, b: int, parents: list[int]) -> None:
 
 
 N = int(input())
-arr = [list(map(int, input().split())) for _ in range(N)]
+# XXX: 정렬하게 되는 경우 원래 행성 번호랑 달라지므로, 행성 번호 저장 필요
+arr = []
+for i in range(N):
+    x, y, z = map(int, input().split())
+    arr.append((i, x, y, z))
 
-# 행성 간 거리 구하기 = O(N^2)
+# 행성 간 거리 구하기
+# X, Y, Z축 각각 정렬하는 경우, 각 축마다 가장 가까운 행성끼리 붙어 있음.
+# (다음 행성 - 현재 행성) 거리를 구하고, 이걸 모든 축에 대해 반복
+# 참고: https://chanhuiseok.github.io/posts/baek-34/
 queue = []
-for a in range(N - 1):
-    for b in range(a + 1, N):
-        costs = []
-        for i in range(3):
-            cost = abs(arr[a][i] - arr[b][i])
-            costs.append(cost)
-        heapq.heappush(queue, (min(costs), a, b))
+for axis in range(1, 4):
+    arr.sort(key=lambda p: p[axis])
+    for i in range(N - 1):
+        cost = arr[i + 1][axis] - arr[i][axis]
+        heapq.heappush(queue, (cost, arr[i][0], arr[i + 1][0]))
 
 # 최소 신장 트리 구하기 (크루스칼 알고리즘)
 parents = [i for i in range(N)]
