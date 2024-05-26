add198885ed263242e3c927b73e227bc954b26b2
Inho <adiens916@naver.com>
2024-05-26 09:42:43 +0900

    19_46 아기 상어

--- problems/This_is_coding_test/1_X/19_46 아기 상어.py
+++ problems/This_is_coding_test/1_X/19_46 아기 상어.py
@@ -1,8 +1,11 @@
+# 반례 및 아이디어 참고: https://velog.io/@fill0006/백준-16236파이썬-아기상어
+# 코드 구성 참고: https://aia1235.tistory.com/35
+
 from collections import deque
 
-
 SHARK = 9
-# 방향은 상좌우하
+# 방향은 상좌우하...가 아녀도 상관 없음.
+# XXX: 우선순위라서 정렬로 해결할 거임.
 dy = (-1, 0, 0, 1)
 dx = (0, -1, 1, 0)
 
@@ -13,62 +16,34 @@ def main():
 
     # 상어 정보
     total_time = 0
-    # 아기 상어 크기는 처음에 2
     size = 2
     eat = 0
     row, col = get_start(area, N)
     area[row][col] = 0
 
     # 가장 가까운 물고기 = BFS
-    # 먹고 나면 다시 해당 지점을 시작점으로 BFS
     while True:
-        visited = [[False] * N for _ in range(N)]
-        visited[row][col] = True
-
-        is_target_eaten = False
-        queue = deque([(row, col, total_time)])
-        while queue:
-            row, col, time = queue.popleft()
-
-            for i in range(4):
-                y = row + dy[i]
-                x = col + dx[i]
-
-                if not (0 <= y < N and 0 <= x < N):
-                    continue
-                if visited[y][x]:
-                    continue
-                visited[y][x] = True
-
-                target = area[y][x]
-                # 크면 못 지나감, 같으면 지나감, 작으면 지나가면서 먹음
-                if target > size:
-                    continue
-                elif target == 0 or target == size:
-                    queue.append((y, x, time + 1))
-                else:  # 0 < target < size
-                    # 잡아먹기
-                    area[y][x] = 0
-                    eat += 1
-                    # 자기 크기만큼 먹으면 크기 += 1
-                    if eat == size:
-                        size += 1
-                        eat = 0
-
-                    # 그 지점부터 다시 시작
-                    row, col = y, x
-                    total_time = time + 1
-                    is_target_eaten = True
-                    break
-
-            # 잡아먹었으면 해당 지점부터 다시 BFS
-            if is_target_eaten:
-                break
-        # 더 이상 먹을 수 없음
-        if not is_target_eaten:
+        targets = find_targets(area, N, row, col, size)
+        # 더 이상 먹을 수 없으면 종료
+        if not any(targets):
+            print(total_time)
             break
 
-    print(total_time)
+        # XXX: 대상을 걸린 시간 - Y축 - X축 순으로 정렬
+        targets.sort()
+        # 가장 가까이 있는 물고기 구하기
+        time, y, x = targets[0]
+        # 잡아 먹기
+        total_time += time
+        area[y][x] = 0
+        eat += 1
+        # 자기 크기만큼 먹으면 크기 += 1
+        if eat == size:
+            size += 1
+            eat = 0
+
+        # 먹고 나면 다시 해당 지점을 시작점으로 BFS
+        row, col = y, x
 
 
 def get_start(area: list[list[int]], n: int) -> tuple[int, int]:
@@ -76,6 +51,43 @@ def get_start(area: list[list[int]], n: int) -> tuple[int, int]:
         for col in range(n):
             if area[row][col] == SHARK:
                 return row, col
+
+
+def find_targets(area, n, row, col, size) -> list[tuple[int, int, int]]:
+    # 잡아 먹을 수 있는 물고기 담아 놓을 배열
+    eatables = []
+
+    visited = [[False] * n for _ in range(n)]
+    visited[row][col] = True
+    time = 0
+
+    queue = deque([(row, col, time)])
+    while queue:
+        row, col, time = queue.popleft()
+
+        for i in range(4):
+            y = row + dy[i]
+            x = col + dx[i]
+
+            if not (0 <= y < n and 0 <= x < n):
+                continue
+            if visited[y][x]:
+                continue
+            visited[y][x] = True
+
+            target = area[y][x]
+            # 크면 못 지나감
+            if target > size:
+                continue
+            # 같으면 지나감
+            elif target == 0 or target == size:
+                queue.append((y, x, time + 1))
+            # 작으면 먹을 수 있음
+            else:
+                eatables.append((time + 1, y, x))
+
+    # (걸린 시간, Y축, X축) 리스트 반환
+    return eatables
 
 
 main()
