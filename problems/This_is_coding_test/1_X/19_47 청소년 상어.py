e1f13885d04908dec12e5bd4f41ab7d7d87a56bf
Inho <adiens916@naver.com>
2024-05-29 06:08:06 +0900

    19_47 청소년 상어 (WIP) (4)

--- problems/This_is_coding_test/1_X/19_47 청소년 상어.py
+++ problems/This_is_coding_test/1_X/19_47 청소년 상어.py
@@ -8,8 +8,8 @@ from copy import deepcopy
 
 
 SHARK = 99
-dy = (0, -1, -1, 0, 1, 1, 1, 0, -1)
-dx = (0, 0, -1, -1, -1, 0, 1, 1, 1)
+dy = (-1, -1, 0, 1, 1, 1, 0, -1)
+dx = (0, -1, -1, -1, 0, 1, 1, 1)
 
 
 def main():
@@ -18,11 +18,11 @@ def main():
         line = list(map(int, input().split()))
         for i in range(8):
             fish = line[i // 2]
-            way = line[i // 2 + 1]
+            way = line[i // 2 + 1] - 1
             fish_arr[row][i // 2] = [fish, way]
 
     shark_way = fish_arr[0][0][1]
-    eat = 0
+    eat = fish_arr[0][0][0]
     shark = ([0, 0], shark_way, eat)
     fish_arr[0][0] = [SHARK, 0]
 
@@ -34,15 +34,30 @@ def dfs(fish_arr: list[list[list[int]]], shark: tuple[list[int], int, int]) -> i
     new_fish_arr = move_fish(fish_arr)
 
     pos, way, eat = shark
+    row, col = pos
+
+    # XXX: max 함수는 비어 있으면 오류 남 -> 임의 값으로 채워넣기
+    result = []
     for i in range(4):
-        y = pos[0] + dy[way]
-        x = pos[1] + dx[way]
+        y = row + dy[way]
+        x = col + dx[way]
 
         if not (0 <= y < 4 and 0 <= x < 4):
             continue
         if new_fish_arr[y][x][0] == 0:
             continue
+
+        fish, fish_way = new_fish_arr[y][x]
+        new_shark = ([y, x], fish_way, eat + fish)
         fish_arr_copy = deepcopy(new_fish_arr)
+        fish_arr_copy[y][x] = [SHARK, 0]
+        fish_arr_copy[row][col] = [0, 0]
+
+        cur_result = dfs(new_fish_arr, new_shark)
+        result.append(cur_result)
+
+    if not any(result):
+        return eat
 
 
 def move_fish(fish_arr):
@@ -72,9 +87,8 @@ def get_fish_order(fish_arr):
 
 def get_next_pos_of_fish(fish_arr, row, col, way):
     for i in range(8):
-        n_way = way + i
-        if n_way == 9:
-            n_way = 1
+        # XXX: 로테이션은 더하기로 하면 실수하니까, 그냥 나머지 연산으로 구하기
+        n_way = (way + i) % 8
 
         y = row + dy[n_way]
         x = col + dx[n_way]
