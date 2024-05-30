fa30ff38bc674688089e52e420025c7e33a0feea
Inho <adiens916@naver.com>
2024-05-30 11:05:00 +0900

    19_47 청소년 상어

--- problems/This_is_coding_test/1_X/19_47 청소년 상어.py
+++ problems/This_is_coding_test/1_X/19_47 청소년 상어.py
@@ -7,7 +7,7 @@ DFS 방식을 쓰되, 현재 맵 상태도 같이 넘겨야 함.
 from copy import deepcopy
 
 
-SHARK = 99
+max_eaten_fish = 0
 dy = (-1, -1, 0, 1, 1, 1, 0, -1)
 dx = (0, -1, -1, -1, 0, 1, 1, 1)
 
@@ -16,76 +16,70 @@ def main():
     fish_arr = [[[0, 0] for x in range(4)] for y in range(4)]
     for row in range(4):
         line = list(map(int, input().split()))
-        for i in range(8):
-            fish = line[i // 2]
-            way = line[i // 2 + 1] - 1
-            fish_arr[row][i // 2] = [fish, way]
+        # XXX: 입력이 잘 됐는지부터 체크하기...
+        for col in range(4):
+            fish = line[col * 2]
+            way = line[col * 2 + 1] - 1
+            fish_arr[row][col] = [fish, way]
 
-    shark_way = fish_arr[0][0][1]
-    eat = fish_arr[0][0][0]
-    shark = ([0, 0], shark_way, eat)
-    fish_arr[0][0] = [SHARK, 0]
-
-    eaten_num = dfs(fish_arr, shark)
-    print(eaten_num)
+    shark = (0, 0)
+    dfs(fish_arr, shark, 0)
+    print(max_eaten_fish)
 
 
-def dfs(fish_arr: list[list[list[int]]], shark: tuple[list[int], int, int]) -> int:
-    new_fish_arr = move_fish(fish_arr)
+def dfs(fish_arr: list[list[list[int]]], shark: tuple[int, int], total_eat: int) -> None:
+    global max_eaten_fish
 
-    pos, way, eat = shark
-    row, col = pos
+    s_row, s_col = shark
+    total_eat += eat_fish(fish_arr, s_row, s_col)
+    move_all_fish(fish_arr, s_row, s_col)
 
-    # XXX: max 함수는 비어 있으면 오류 남 -> 임의 값으로 채워넣기
-    result = []
-    for i in range(4):
-        y = row + dy[way]
-        x = col + dx[way]
-
-        if not (0 <= y < 4 and 0 <= x < 4):
-            continue
-        if new_fish_arr[y][x][0] == 0:
-            continue
-
-        fish, fish_way = new_fish_arr[y][x]
-        new_shark = ([y, x], fish_way, eat + fish)
-        fish_arr_copy = deepcopy(new_fish_arr)
-        fish_arr_copy[y][x] = [SHARK, 0]
-        fish_arr_copy[row][col] = [0, 0]
-
-        cur_result = dfs(new_fish_arr, new_shark)
-        result.append(cur_result)
-
-    if not any(result):
-        return eat
+    # XXX: 후보 구하기 & 더 이상 후보 없으면 종료
+    candidates = get_fish_candidates(fish_arr, s_row, s_col)
+    if len(candidates) == 0:
+        # XXX: 현재 최대 vs. 이번 분기까지 넘어온 값 비교
+        max_eaten_fish = max(max_eaten_fish, total_eat)
+        return
+    for y, x in candidates:
+        new_arr = deepcopy(fish_arr)
+        # 다음 분기로 값 넘기기
+        dfs(new_arr, (y, x), total_eat)
 
 
-def move_fish(fish_arr):
-    fish_order = get_fish_order(fish_arr)
+def eat_fish(fish_arr, row, col) -> int:
+    fish = fish_arr[row][col][0]
+    fish_arr[row][col][0] = -1
+    return fish
+
+
+def move_all_fish(fish_arr, s_row, s_col):
     for i in range(1, 17):
-        row, col = fish_order[i]
+        # XXX: 물고기 위치가 계속 바뀌니까, 그냥 매번 찾는 게 더 쉬움.
+        row, col = find_fish(fish_arr, i)
+        # 해당 번호가 없으면 넘어감.
+        if row == -1:
+            continue
         fish, way = fish_arr[row][col]
 
-        y, x = get_next_pos_of_fish(fish_arr, row, col, way)
+        # 다음에 갈 수 있는 곳 찾기
+        # XXX: 방향 바뀐 걸로 쓰기
+        y, x, way = get_next_pos_way_of_fish(row, col, way, s_row, s_col)
+        # 서로 위치 바꾸기
         n_fish, n_way = fish_arr[y][x]
-        if n_fish != 0:
-            fish_arr[row][col] = [n_fish, n_way]
         fish_arr[y][x] = [fish, way]
-    return fish_arr
+        fish_arr[row][col] = [n_fish, n_way]
 
 
-def get_fish_order(fish_arr):
-    fish_order = [(0, 0) for _ in range(17)]
+def find_fish(fish_arr, i):
     for row in range(4):
         for col in range(4):
-            fish, way = fish_arr[row][col]
-            if fish == SHARK:
-                continue
-            fish_order[fish] = (row, col)
-    return fish_order
+            if fish_arr[row][col][0] == i:
+                return row, col
+    # XXX: 이미 먹어서 없는 경우도 고려
+    return -1, -1
 
 
-def get_next_pos_of_fish(fish_arr, row, col, way):
+def get_next_pos_way_of_fish(row, col, way, s_row, s_col):
     for i in range(8):
         # XXX: 로테이션은 더하기로 하면 실수하니까, 그냥 나머지 연산으로 구하기
         n_way = (way + i) % 8
@@ -95,9 +89,30 @@ def get_next_pos_of_fish(fish_arr, row, col, way):
 
         if not (0 <= y < 4 and 0 <= x < 4):
             continue
-        if fish_arr[y][x] == SHARK:
+        # XXX: 상어인지는 좌표 '값' 대신에 그냥 좌표로 비교하는 게 제일 쉬움
+        # 좌표 '값'으로 비교하면, 상어가 옮겨 다닐 떄마다 좌표 값을 갱신해야 해서 매우 복잡해짐.
+        if y == s_row and x == s_col:
             continue
-        return y, x
+
+        # XXX: 방향 바뀌니까, 방향도 반환하기
+        return y, x, n_way
+
+
+def get_fish_candidates(fish_arr, s_row, s_col):
+    s_way = fish_arr[s_row][s_col][1]
+
+    cand = []
+    for i in range(4):
+        # XXX: 1씩 늘려 가야 함
+        s_row += dy[s_way]
+        s_col += dx[s_way]
+
+        if not (0 <= s_row < 4 and 0 <= s_col < 4):
+            continue
+        if fish_arr[s_row][s_col][0] == -1:
+            continue
+        cand.append((s_row, s_col))
+    return cand
 
 
 main()
@@ -108,3 +123,21 @@ main()
 6 1 13 6 4 3 11 4
 16 1 8 7 5 2 12 2
 """  # 33
+"""
+16 7 1 4 4 3 12 8
+14 7 7 6 3 4 10 2
+5 2 15 2 8 3 6 4
+11 8 2 4 13 5 9 4
+"""  # 43
+"""
+12 6 14 5 4 5 6 7
+15 1 11 7 3 7 7 5
+10 3 8 3 16 6 1 1
+5 8 2 7 13 6 9 2
+"""  # 76
+"""
+2 6 10 8 6 7 9 4
+1 7 16 6 4 2 5 8
+3 7 8 6 7 6 14 8
+12 7 15 4 11 3 13 3
+"""  # 39
