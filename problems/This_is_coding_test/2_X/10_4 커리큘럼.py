5d8351f79c9164bfcc05129cc2fa42f7a6e5c70d
Inho <adiens916@naver.com>
2024-06-17 10:27:48 +0900

    10_4 커리큘럼 (개선)

--- problems/This_is_coding_test/2_X/10_4 커리큘럼.py
+++ problems/This_is_coding_test/2_X/10_4 커리큘럼.py
@@ -1,11 +1,11 @@
+# 참고: https://imhihi.tistory.com/entry/이코테-09-그래프-이론-커리큘럼
 from collections import deque
-
 
 N = int(input())
 
 nodes = [0] * (N + 1)
-next_nodes = [[] for _ in range(N + 1)]
-prev_nodes = [[] for _ in range(N + 1)]
+# XXX: 다음 노드로 가는 배열은 필수. 그래야 in_degree 체크 가능.
+graph: list[list[int]] = [[] for _ in range(N + 1)]
 in_degrees = [0] * (N + 1)
 queue = deque()
 
@@ -14,27 +14,28 @@ for i in range(1, N + 1):
     lec_time, *prev_list, _ = lec_info
 
     nodes[i] = lec_time
-    for v in prev_list:
-        prev_nodes[i].append(v)
-        next_nodes[v].append(i)
+    for prev in prev_list:
+        # 이전 노드로 가는 건 필요 없음.
+        graph[prev].append(i)
         in_degrees[i] += 1
     if len(prev_list) == 0:
         queue.append(i)
 
-
+# XXX: 값 저장할 배열 초기화
+table = [t for t in nodes]
+# 위상 정렬
 while queue:
-    node = queue.popleft()
+    start = queue.popleft()
 
-    for v in next_nodes[node]:
-        in_degrees[v] -= 1
-        if in_degrees[v] == 0:
-            prev_max = max([nodes[i] for i in prev_nodes[v]])
-            nodes[v] += prev_max
-            queue.append(v)
+    for next_ in graph[start]:
+        # XXX: 항상 체크하면서 더 큰 값을 저장 (DP)
+        table[next_] = max(table[next_], table[start] + nodes[next_])
 
+        in_degrees[next_] -= 1
+        if in_degrees[next_] == 0:
+            queue.append(next_)
 
 print(*nodes[1:], sep='\n')
-
 
 """
 5
