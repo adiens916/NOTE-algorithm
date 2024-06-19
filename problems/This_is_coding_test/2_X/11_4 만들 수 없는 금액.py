1054d2ee4fa20f485ab8ce49889c3e0d63f86bfd
Inho <adiens916@naver.com>
2024-06-20 06:54:38 +0900

    11_4 만들 수 없는 금액 (WIP)

--- problems/This_is_coding_test/2_X/11_4 만들 수 없는 금액.py
+++ problems/This_is_coding_test/2_X/11_4 만들 수 없는 금액.py
@@ -1,19 +1,31 @@
+"""
+1. 1부터 X까지 만들 수 있다고 하자.
+2. 만약 새로운 동전 Y가 추가된다고 하면,
+추가로 1+Y부터 X+Y까지 만들 수 있음.
+3. 1과 2의 범위를 합치면 [1, X] + [1+Y, X+Y]
+
+4. 위 두 범위가 합쳐지기 위해선,
+1+Y가 X+1이랑 같거나, 더 작아야 함.
+즉, 1+Y <= X+1
+Y <= X
+
+5. 반대로 Y > X인 경우엔 위 범위가 이어지지 않고 떨어짐.
+6. 즉, 새로 추가된 동전 Y가 X보다 크면,
+이어지지 않으므로, X+1이 답임.
+"""
+
 N = int(input())
 coins = list(map(int, input().split()))
-coins.sort(reverse=True)
+coins.sort()
 
-MAX = 1000000
-for i in range(1, MAX + 1):
-    remains = i
-
-    for coin in coins:
-        if coin > remains:
-            continue
-        remains -= coin
-
-    if remains != 0:
-        print(i)
+max_ = 1
+for coin in coins:
+    if max_ < coin:
         break
+    else:
+        max_ += coin
+
+print(max_)
 
 """
 5
