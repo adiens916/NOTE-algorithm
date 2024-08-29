16ffec77e49d47da74d6c3497df74ae1ffb255a4
Inho <adiens916@naver.com>
2024-08-30 06:50:40 +0900

    13_19 연산자 끼워 넣기 (중복 계산 이유 분석)

--- problems/This_is_coding_test/2_X/13_19 연산자 끼워 넣기.py
+++ problems/This_is_coding_test/2_X/13_19 연산자 끼워 넣기.py
@@ -27,35 +27,32 @@ def permutation(k, cur_num):
         min_val = min(min_val, cur_num)
         return
     else:
+        # XXX: for 문으로 하면 안 됨.
+        # for 문에 들어간 범위 값은 고정되므로, 무조건 해당 값만큼 반복.
+        # 그래서 실제보다 더 많이 반복해서 계산하게 됨 (중복)
+        # => 실제 값 참조하는 리스트 & if 문 이용해야 함.
         for _ in range(add):
-            cur_num += nums[k + 1]
             add -= 1
-            permutation(k + 1, cur_num)
+            permutation(k + 1, cur_num + nums[k + 1])
             add += 1
-            cur_num -= nums[k + 1]
         for _ in range(sub):
-            cur_num -= nums[k + 1]
             sub -= 1
-            permutation(k + 1, cur_num)
+            permutation(k + 1, cur_num - nums[k + 1])
             sub += 1
-            cur_num += nums[k + 1]
         for _ in range(mul):
-            cur_num *= nums[k + 1]
             mul -= 1
-            permutation(k + 1, cur_num)
+            permutation(k + 1, cur_num * nums[k + 1])
             mul += 1
-            cur_num //= nums[k + 1]
         for _ in range(div):
-            origin = cur_num
             if cur_num < 0:
-                cur_num = -(-cur_num // nums[k + 1])
+                next_num = -(-cur_num // nums[k + 1])
             else:
-                cur_num //= nums[k + 1]
+                next_num = cur_num // nums[k + 1]
             div -= 1
-            permutation(k + 1, cur_num)
+            # XXX: 원본 값을 내려 보내지 말고, 복사본을 수정해서 전달하기
+            # 그러면 원본 값을 다시 복구하느라, 필요없는 연산을 안 해도 됨.
+            permutation(k + 1, next_num)
             div += 1
-            # XXX: 나눗셈의 몫만 취했으므로, 그대로 곱하면 나머지가 손실됨. 원본 값 보존 필요.
-            cur_num = origin
 
 
 permutation(0, nums[0])
